958a8da9515150fcaabc490472cf0ecc0daf0eb2
Black-Ser <97171833+Black-Ser@users.noreply.github.com>
2023-01-15 21:04:44 +0530

    Update carbon.py

--- plugins/ExraMods/carbon.py
+++ plugins/ExraMods/carbon.py
@@ -41,8 +41,8 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**Made by @MatRixBotzTG**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/MatRixBotzTG")]]),                   
+        caption="**Made by @HyperBotz**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/HyperBotz")]]),                   
     )
     await m.delete()
     carbon.close()
