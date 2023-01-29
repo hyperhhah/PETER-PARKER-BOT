cfb220e466be9dcb3ace13353933ec51b706e827
Black-Ser <97171833+Black-Ser@users.noreply.github.com>
2023-01-29 14:04:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,6 +248,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('𝗢𝗧𝗧 𝗠𝗢𝗩𝗜𝗘𝗦', url='https://t.me/C1nemaCorner') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
